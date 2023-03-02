03cb4f0f8c87143d138e5463dd5948f4a882b171
Kaleb McDonnell <kalebmcdonnell24@gmail.com>
2023-03-02 09:14:34 -0600

    Git Functionality

--- src/com/cognixia/jump/movietracker/MovieTracker.java
+++ src/com/cognixia/jump/movietracker/MovieTracker.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class MovieTracker {
 	public static void main(String[] args) {
 		
+		System.out.println("Hello");
 		login();
+		
 		
 	}
 	public static void login() {
