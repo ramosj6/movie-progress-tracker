5524abb9179a57f8cf46929153de0409f006c0ea
viveroa2291 <viveroa2291@uwec.edu>
2023-03-02 13:18:01 -0600

    Fixes
    Cleaning up the main

--- src/com/cognixia/jump/movietracker/MovieTracker.java
+++ src/com/cognixia/jump/movietracker/MovieTracker.java
@@ -1,10 +1,6 @@
 package com.cognixia.jump.movietracker;
 
 import java.io.IOException;
-
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -19,22 +15,13 @@ import com.cognixia.jump.dao.MovieDaoSql;
 public class MovieTracker {
 	public static void main(String[] args) {
 		
-		MovieDaoSql movie = new MovieDaoSql();
-		
-	try {
-			movie.setConnection();
-		
 		if(login()) { 
 			System.out.println("Login Successful");
-			System.out.println("Hello");
 			menu();
 		}
 		else {
 			System.out.println("Login Failed, try again.");
 		}
-	} catch (ClassNotFoundException | IOException | SQLException e) {
-		System.out.println("Can't establish connection, quitting program");
-	}
 	}
 	public static boolean login() {
 		String username, password;
@@ -46,7 +33,7 @@ public class MovieTracker {
 		System.out.println();
 		System.out.print("Password: ");
 		password = sc.next();
-		//System.out.println();
+		System.out.println();
 		System.out.println("Thanks for entering you username " + username + " we will verify if that account exists.");
 		System.out.println("-------------------------------------------------------------------------------------------");
 
